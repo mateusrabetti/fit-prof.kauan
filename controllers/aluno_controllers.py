c531ca9012d24da78b720954ee1c8c998bd6887d
mateusrabetti <31521@raphaeldisanto.com.br>
2026-08-13 22:31:42 -0300

    Add files via upload

--- controllers/aluno_controllers.py
+++ controllers/aluno_controllers.py
@@ -1,27 +1,27 @@
 import json
-from models.cachorro_models import cachorro   
+from models.aluno_models import aluno   
 from db import db                        
 from flask import make_response
 
-def get_cachorro():
-    cachorro = cachorro.query.all()  
+def get_aluno():
+    aluno = aluno.query.all()  
     response = make_response(
         json.dumps({
-            'mensagem': 'Lista de cachorro.',
-            'dados': [cachorro.json() for cachorro in cachorro]  
+            'mensagem': 'Lista de aluno.',
+            'dados': [aluno.json() for aluno in aluno]  
         }, ensure_ascii=False, sort_keys=False)  
     )
     response.headers['Content-Type'] = 'application/json'  
     return response
 
-def get_cachorro_by_id(cachorro_id):
-    cachorro = cachorro.query.get(cachorro_id)  
+def get_aluno_by_id(aluno_id):
+    aluno = aluno.query.get(aluno_id)  
 
-    if cachorro: 
+    if aluno: 
         response = make_response(
             json.dumps({
-                'mensagem': 'cachorro encontrado.',
-                'dados': cachorro.json()  
+                'mensagem': 'aluno encontrado.',
+                'dados': aluno.json()  
             }, ensure_ascii=False, sort_keys=False)
         )
         response.headers['Content-Type'] = 'application/json'  
@@ -29,26 +29,26 @@ def get_cachorro_by_id(cachorro_id):
     else:
         
         response = make_response(
-            json.dumps({'mensagem': 'cachorro não encontrado.', 'dados': {}}, ensure_ascii=False),
+            json.dumps({'mensagem': 'aluno não encontrado.', 'dados': {}}, ensure_ascii=False),
             404  
         )
         response.headers['Content-Type'] = 'application/json'  
         return response
         
-def create_cachorro(cachorro_data):           
-    novo_cachorro = cachorro(  
+def create_aluno(aluno_data):           
+    novo_aluno = aluno(  
                        
-        nome=cachorro_data['nome'],     
-        raca=cachorro_data['raca'],       
-        idade=cachorro_data['idade']
+        nome=aluno_data['nome'],     
+        raca=aluno_data['raca'],       
+        idade=aluno_data['idade']
                              
     )
-    db.session.add(novo_cachorro)          
+    db.session.add(novo_aluno)          
     db.session.commit()                  
     response = make_response(            
         json.dumps({                      
-            'mensagem': 'cachorro cadastrado com sucesso.',  
-            'cachorro': novo_cachorro.json()   
+            'mensagem': 'aluno cadastrado com sucesso.',  
+            'aluno': novo_aluno.json()   
         }, sort_keys=False)               
     )
     response.headers['content-Type'] = 'application/json'  
